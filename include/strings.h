9a16f296b80ed18091ae1d2f70787f8e93de8ea1
00mn00 <trevor.k.collingwood@gmail.com>
2022-05-31 21:32:07 +0100

    Move Items Between Pokemon
    Credit to AsparagusEduardo @ InfusedEmerald

--- include/strings.h
+++ include/strings.h
@@ -1649,4 +1649,9 @@ extern const u8 gText_HowDoIBattleThem[];
 extern const u8 gText_AboutTitles[];
 extern const u8 gText_Nothing[];
 
+extern const u8 gMenuText_Move[];
+extern const u8 gText_MoveItemWhere[];
+extern const u8 gText_XsYAnd[];
+extern const u8 gText_XsYWereSwapped[];
+
 #endif //GUARD_STRINGS_H
--- src/strings.c
+++ src/strings.c
@@ -1315,3 +1315,8 @@ const u8 gText_WhoAreThey[] = _("Who are they?");
 const u8 gText_HowDoIBattleThem[] = _("How do I battle them?");
 const u8 gText_AboutTitles[] = _("About titles");
 const u8 gText_Nothing[] = _("Exit");
+
+const u8 gMenuText_Move[] = _("MOVE");
+const u8 gText_MoveItemWhere[] = _("Move item to which POKéMON?");
+const u8 gText_XsYAnd[] = _("{STR_VAR_1}'s\n{STR_VAR_2} and\l");
+const u8 gText_XsYWereSwapped[] = _("{STR_VAR_1}'s\l{STR_VAR_2} were swapped!{PAUSE_UNTIL_PRESS}");
